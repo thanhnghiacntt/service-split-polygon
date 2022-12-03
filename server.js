a9e3073829c2425568fb1bcbdb187bdd8c4a0c9f
Nguyen Thanh Nghia <thanhnghiacntt@gmail.com>
2022-12-03 14:55:53 +0700

    Update source

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
+'use strict';
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const turf = require('@turf/turf');
 const app = express();
+var polylabel = require('./polylabel');
 app.use(bodyParser.urlencoded({
         extended: true
     }));
@@ -69,6 +72,32 @@ app.post('/', (req, res) => {
             geojson.features.push(e)
         });
         res.json(geojson);
+    } catch (e) {
+        console.log(inputFeature);
+        res.json(null);
+    }
+});
+app.post('/center', (req, res) => {
+    var inputFeature = req.body;
+    try {
+        var polygon = null;
+        var hasReponse = false;
+        if(inputFeature.geometry.type == "Polygon"){
+            polygon = inputFeature.geometry.coordinates;
+        }else if(inputFeature.geometry.type == "MultiPolygon"){
+            polygon = inputFeature.geometry.coordinates[0];
+        }
+        if(polygon != null){
+            var p = polylabel(polygon, 0.0000001, false);
+            if(p.length >= 2){
+                hasReponse = true;
+                console.log(p);
+                res.json({"lng":p[0], "lat":p[1]});
+            }
+        }
+        if(!hasReponse){
+            res.json({"error":"Không tìm được"});
+        }
     } catch (e) {
         console.log(inputFeature);
         res.json(null);
